ec9731f80d9a94137380b25c6b2ce0b8a1283d1d
David Cooper <dmcooper2412@gmail.com>
2026-01-18 21:13:42 -0800

    Slowly making banner re-usable

--- Assets/Scripts/Speaker.cs
+++ Assets/Scripts/Speaker.cs
@@ -1,14 +1,27 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Speaker : MonoBehaviour
 {
 
     private Canvas _helpCanvas;
+    private InputAction _actionPressed;
+    private bool _isBannerOpen;
 
     void Start()
     {
         _helpCanvas = gameObject.GetComponentInChildren<Canvas>();
+        _actionPressed = InputSystem.actions.FindAction("Interact");
+    }
+
+    void Update()
+    {
+        if (_actionPressed.IsPressed() && _helpCanvas.enabled && !_isBannerOpen)
+        {
+            Debug.Log("This will open the menu.");
+            _isBannerOpen = true;
+        }
     }
     
     private void OnTriggerEnter2D(Collider2D other)
@@ -19,5 +32,6 @@ public class Speaker : MonoBehaviour
     private void OnTriggerExit2D(Collider2D other)
     {
         _helpCanvas.enabled = false;
+        _isBannerOpen = false;
     }
 }
---
+++ Assets/Scripts/SpeechBanner.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SpeechBanner : MonoBehaviour
+{
+
+    [SerializeField] private SpeakerSpeech speakerSpeech;
+
+    private InputAction _enterButton;
+    private readonly Dictionary<SpeakerSpeech, string[]> _speakerSpeeches = new ()
+    {
+        {SpeakerSpeech.TutorialStage1, new []{ "Hi there, Zero! Welcome to the bottom!", "Try using the W,A,S,D keys to move around!" }},
+        {SpeakerSpeech.TutorialStage2, new []{ "You made it up here. Great!", "There's a locked gate up ahead, but you can turn it off by standing on that switch.", "There are also some blocks you can push around. Give it a try!" }},
+        {SpeakerSpeech.TutorialStage3, new []{ "You made it past the gate! Awesome!", "There's another locked gate, but you can't move this block.", "Try pressing <space> when the yellow target appears, oh the block to the left, to SWAP your value." }},
+        {SpeakerSpeech.TutorialStage4, new []{ "Wow! You're fast at this!", "Unfortunately to ascend to the next level, you have to provide \"value\".", "Swap near the teleporter to provide your value." }}
+    };
+
+
+    public string[] GetDialogue(SpeakerSpeech speech)
+    {
+        return _speakerSpeeches[speech];
+    }
+}
+
+public enum SpeakerSpeech
+{
+    TutorialStage1,
+    TutorialStage2,
+    TutorialStage3,
+    TutorialStage4,
+}
